aff4f8acfb6f5e1509f22296c1a209ecb5fe1bae
Mehedi Hasan Antor <antordon500@gmail.com>
2024-04-02 23:25:03 +0600

    Completed

--- Watermelon.java
+++ Watermelon.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 public class Watermelon {
 
-    
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        try{
+            int weight = input.nextInt();
+
+        if (weight%2==1) {
+            System.out.printf("NO");
+        }else{
+            if (weight==2) {
+                System.out.println("NO");
+            }else
+            System.out.println("YES");
+        }
+    }finally{
+        input.close();
+    }
+}
 }
